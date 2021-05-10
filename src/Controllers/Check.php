aef327767aec5d2de51214b2034e003a6fbad13c
daycry <daycry9@gmail.com>
2021-05-10 18:43:08 +0200

    Update Check.php

---
+++ src/Controllers/Check.php
@@ -0,0 +1,53 @@
+<?php
+namespace Daycry\Maintenance\Controllers;
+
+use CodeIgniter\Controller;
+use Config\Services;
+use Daycry\Maintenance\Libraries\IpUtils;
+use Daycry\Maintenance\Exceptions\ServiceUnavailableException;
+
+class MaintenanceMode extends Controller
+{
+    private $config;
+
+    public function __construct(){}
+
+    public static function check()
+    {
+        // if request is from CLI
+        if(is_cli()) return true;
+
+        $config = \Daycry\Maintenance\Libraries\Config::getConfig();
+
+        $donwFilePath = $config->FilePath . $config->FileName;
+
+        // if donw file does not exist app should keep running
+        if( !file_exists( $donwFilePath ) )
+        {
+            return true;
+        }
+
+        // get all json data from donw file
+        $data = json_decode( file_get_contents( $donwFilePath ), true );
+
+        // if request ip was entered in allowed_ips
+        // the app should continue running
+        $lib = new IpUtils();
+        if( $lib->checkIp( Services::request()->getIPAddress(), $data[ "allowed_ips" ] ) )
+        {
+            return true;
+        }
+
+        // if user's browser has been used the cookie pass
+        // the app should continue running
+        helper( 'cookie' );
+        $cookieName = get_cookie( $data[ "cookie_name" ] );
+
+        if( $cookieName == $data[ "cookie_name" ] )
+        {
+            return true;
+        }
+
+        throw ServiceUnavailableException::forServerDow( $data[ "message" ] );
+    }
+}
