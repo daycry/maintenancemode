ff8079aed22334ca1e3d4a569900d953f5d7c483
Jordi de la Mano <jmano@mediapro.tv>
2022-08-16 10:54:52 +0200

    - PHPUnit

--- tests/Maintenance/MaintenanceTest.php
+++ tests/Maintenance/MaintenanceTest.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace Daycry\Maintenance;
+namespace Tests\Maintenance;
 
-class MaintenanceTest extends TestCase
+use CodeIgniter\Test\CIUnitTestCase;
+
+class MaintenanceTest extends CIUnitTestCase
 {
     private $message = 'In maintenance';
     private $ip = '127.0.0.1';
@@ -12,40 +14,98 @@ class MaintenanceTest extends TestCase
         parent::setUpBeforeClass();
     }
 
-    public function testInMaintenance()
+    public function testCommandInMaintenanceDown()
     {
         $config = config( 'Daycry\Maintenance\Config\Maintenance' );
 
-        command( 'mm:down -message "'. $this->message .'" -ip ' . $this->ip );
+        ob_start();
+        $output = command( 'mm:down -message "'. $this->message .'" -ip ' . $this->ip );
         
-        $this->assertFileExists( 
-            $config->filePath . $config->fileName, 
-            "given filename doesn't exists"
-        ); 
+        if (!$output) {
+            $output = \ob_get_contents();
+        }
+        ob_end_clean();
+
+        $this->assertFileExists( $config->filePath . $config->fileName );
     }
 
-    public function testCheckStatus()
+    public function testCommandInMaintenanceAlreadyDown()
     {
         $config = config( 'Daycry\Maintenance\Config\Maintenance' );
 
-        command( 'mm:status' );
+        ob_start();
+        $output = command( 'mm:down -message "'. $this->message .'" -ip ' . $this->ip );
         
-        $this->assertFileExists( $config->filePath . $config->fileName, "given filename doesn't exists" );
+        if (!$output) {
+            $output = \ob_get_contents();
+        }
+        ob_end_clean();
+
+        $this->assertFileExists( $config->filePath . $config->fileName );
+    }
+
+    public function testCommandCheckStatusDown()
+    {
+        $config = config( 'Daycry\Maintenance\Config\Maintenance' );
+
+        ob_start();
+        $output = command( 'mm:status' );
+        
+        if (!$output) {
+            $output = \ob_get_contents();
+        }
+        ob_end_clean();
 
         $data = \json_decode( file_get_contents( $config->filePath . $config->fileName ), true );
 
+        $this->assertFileExists( $config->filePath . $config->fileName );
         $this->assertEquals( $this->message, $data[ 'message'] );
-
         $this->assertTrue( $this->_arrays_are_similar( \explode( ' ', $this->ip ), $data[ 'allowed_ips' ] ) );
     }
 
-    public function testOutMaintenance()
+    public function testCommandMaintenanceUp()
     {
         $config = config( 'Daycry\Maintenance\Config\Maintenance' );
 
-        command( 'mm:up' );
+        ob_start();
+        $output = command( 'mm:up' );
         
-        $this->assertFileNotExists( $config->filePath . $config->fileName, "given filename does exists" ); 
+        if (!$output) {
+            $output = \ob_get_contents();
+        }
+        ob_end_clean();
+
+        $this->assertFileDoesNotExist( $config->filePath . $config->fileName);
+    }
+
+    public function testCommandMaintenanceAlreadyUp()
+    {
+        $config = config( 'Daycry\Maintenance\Config\Maintenance' );
+
+        ob_start();
+        $output = command( 'mm:status' );
+        
+        if (!$output) {
+            $output = \ob_get_contents();
+        }
+        ob_end_clean();
+
+        $this->assertFileDoesNotExist( $config->filePath . $config->fileName);
+    }
+
+    public function testCommandInMaintenanceDownWithoutMessage()
+    {
+        $config = config( 'Daycry\Maintenance\Config\Maintenance' );
+
+        ob_start();
+        $output = command( 'mm:down' );
+        
+        if (!$output) {
+            $output = \ob_get_contents();
+        }
+        ob_end_clean();
+
+        $this->assertFileExists( $config->filePath . $config->fileName );
     }
 
     /**
--- tests/Maintenance/TestCase.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Daycry\Maintenance;
-
-class TestCase extends \PHPUnit\Framework\TestCase
-{
-}
